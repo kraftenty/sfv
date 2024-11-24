0829e58f59c90da833694cd19c88de459a2f627b
sobogil <69839941+sobogil@users.noreply.github.com>
2024-11-24 20:22:32 +0900

    더미 파일 생성기로 측정 가능

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 명령어 자동화를 위한 리스트
         List<String[]> commands = new ArrayList<>();
         commands.add(new String[]{"init"});
@@ -13,7 +14,8 @@ public class Main {
 //        commands.add(new String[]{"status"});
 //        commands.add(new String[]{"commit", "-m", "second commit"});
 //        commands.add(new String[]{"status"});
-
+        DummyFileGenerator generator = new DummyFileGenerator();
+        generator.generateFiles(50,50);
 
         commands.add(new String[]{"log"});
 
